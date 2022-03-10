fd4390d035c2e38b55e7af7cedbedf60378fc0e8
p-lacerda <xcloudgameemail@gmail.com>
2022-03-10 15:54:23 -0300

    Inicia o projeto e termina o requisito 1

---
+++ src/Races/Dwarf.ts
@@ -0,0 +1,17 @@
+// import Race from ".";
+
+// class Dwarf extends Race {
+//   constructor(name: string, dexterity: number, _maxLifePoints: number = 80) { 
+//     super(name, dexterity);
+//   }
+
+//   get maxLifePoints(): number {
+//     return this.maxLifePoints;
+//   }
+
+//   static createdRacesInstances(): number {
+//     return 2;
+//   }
+// }
+
+// export { Dwarf };
